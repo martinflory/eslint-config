3752defc680328a4a484b31fd480f1c8ad4dc178
Adam Charron <adam@charrondev.com>
2018-02-02 10:34:18 -0500

    Add eslint plugin react

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 module.exports = {
     "parser": "babel-eslint",
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "plugins": [
+        "react"
+    ],
     "parserOptions": {
         "ecmaVersion": 2017,
         "sourceType": "module",
